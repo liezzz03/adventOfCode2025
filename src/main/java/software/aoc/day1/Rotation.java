27f00c2bce5d65d5ed3280e18a4dbd803f1efeae
Liez <paulalijunuezhernandez@gmail.com>
2026-06-20 11:26:13 +0100

    Day 1 Part 1

---
+++ src/main/java/software/aoc/day1/Rotation.java
@@ -0,0 +1,20 @@
+package software.aoc.day1;
+
+public record Rotation(int step) {
+    public static Rotation from(String order) {
+        return orderIn(order);
+    }
+
+    private static Rotation orderIn(String order) {
+        return new Rotation(signOf(order) * valueOf(order));
+    }
+
+    private static int valueOf(String order) {
+        return Integer.parseInt(order.substring(1));
+    }
+
+    private static int signOf(String order) {
+        return order.charAt(0) == 'L' ? -1 : 1;
+    }
+
+}
